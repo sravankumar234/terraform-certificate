447c88e0c4e6f623baa171eac7f9d76ed93caba0
sukeshdonepudi <sukeshdonepudi@gmail.com>
2023-05-07 20:41:29 -0400

    GCp wirh simple bucket and VPC with main and test workspace adding backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-# terraform {
-#   backend "gcs" {
-#     bucket  = "state-files-9020"
-#     prefix  = "terraform/state"
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket  = "state-files-9020"
+    prefix  = "terraform/state"
+  }
+}
